8048ef8460a103b60884ba39be2891b01b62bb8a
Vladyslav Halushkin <v.halushkin@gmail.com>
2019-03-01 17:26:25 +0200

    Updates

---
+++ precache-manifest.8db1326f1a58b21b36a5bb35ce0762b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "520fed621e5bb3025509",
+    "url": "/React-Router-Practice/static/js/runtime~main.520fed62.js"
+  },
+  {
+    "revision": "44c323e3e2e55b4ebb32",
+    "url": "/React-Router-Practice/static/js/main.44c323e3.chunk.js"
+  },
+  {
+    "revision": "4588c43267b75e696a36",
+    "url": "/React-Router-Practice/static/js/2.4588c432.chunk.js"
+  },
+  {
+    "revision": "44c323e3e2e55b4ebb32",
+    "url": "/React-Router-Practice/static/css/main.08a16098.chunk.css"
+  },
+  {
+    "revision": "ae22aff84a9a40fdd340614f4ada8299",
+    "url": "/React-Router-Practice/index.html"
+  }
+];
